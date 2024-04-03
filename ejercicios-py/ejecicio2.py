bdce3863de3244113f08be1ea8efe5b0f5c6650c
Albert21Arg <116925535+Albert21Arg@users.noreply.github.com>
2024-04-03 12:03:26 -0500

    Create ejecicio2.py
    Se realiza el ejercico 2 py

---
+++ ejercicios-py/ejecicio2.py
@@ -0,0 +1,9 @@
+# 2 Promedio de 3 notas para un estudiante.
+print('digitar nota 1')
+n1 = float(input())
+print('digitar nota 2')
+n2 = float(input())
+print('digitar nota 3')
+n3 = float(input())
+promedio = (n1+n2+n3)/3
+print(f'el promedio de nota es: {promedio} ')
