58b818de49742b5ab47ec0c0f7176c3d17565e70
Albert Taborda <alberttaborda@hotmail.com>
2024-04-15 18:55:34 -0500

    Se modifica ejercicio 31

--- ejercicios-py/ejercicio31.py
+++ ejercicios-py/ejercicio31.py
@@ -1,19 +1,24 @@
-#31
-numero = int(input('Digitar Numero:'))
-meses = {
-    1: 'Enero',
-    2: 'Febrero',
-    3: 'Marzo',
-    4: 'Abril',
-    5: 'Mayo',
-    6: 'Junio',
-    7: 'Julio',
-    8: 'Agosto',
-    9: 'Septiembre',
-    10: 'Octubre',
-    11: 'Noviembre',
-    12: 'Diciembre'
-}
-if numero in meses:
-    mes = meses[numero]
-    print(f'El numero digitado corresponde al mes de - {mes} -')
+try:
+    numero_inicial = input('Digitar Numero 1 - 12:')
+    numero = int(numero_inicial)
+    meses = {
+       1: 'Enero',
+       2: 'Febrero',
+       3: 'Marzo',
+       4: 'Abril',
+       5: 'Mayo',
+       6: 'Junio',
+       7: 'Julio',
+       8: 'Agosto',
+       9: 'Septiembre',
+       10: 'Octubre',
+       11: 'Noviembre',
+       12: 'Diciembre'
+    }
+    if numero in meses:
+        mes = meses[numero]
+        print(f'El numero digitado corresponde al mes de - {mes} -')
+    elif numero not in range(1, 13):
+        print('Numero Fuera de rango')
+except ValueError:
+    print('Error: El dato ingresado no es un numero')
