75d7ade8b9cb99d8d69fda6b3bfdc8db9943b6ee
Albert <alberttaborda@hotmail.com>
2024-06-24 19:20:06 -0500

    Taller 1c

--- ejercicios-py/taller-1c/ejercicio-6.py
+++ ejercicios-py/taller-1c/ejercicio-6.py
@@ -1,5 +1,5 @@
 #Ordenar Lista de Palabras por Longitud
 #Escribe un algoritmo que ordene una lista de palabras por su longitud.
-palabras = ["python", "es", "un", "lenguaje", "de", "prigramacion"]
+palabras = ["python", "es", "un", "lenguaje", "de", "programacion"]
 palabras_ordenadas = sorted(palabras, key=len)
 print(f"Palabras ordenadas por longitud: {palabras_ordenadas}")
