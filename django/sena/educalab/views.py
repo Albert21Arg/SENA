059bd2647749cd335bb34fc28d0ab095d91a166e
Albert Taborda <alberttaborda@hotmail.com>
2024-09-12 15:54:20 -0500

    modificaciones de djanjo

--- django/sena/educalab/views.py
+++ django/sena/educalab/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def saludar(request):
     print("Hola mundo...!")
     return HttpResponse("Hola SENA..!")
@@ -14,13 +12,13 @@ def saludar_personalizado(request, nombre):
     return HttpResponse(f"Hola {nombre}")
 
 def suma(request, num1, num2):
-    return HttpResponse(f"Resultado: {num1+num2}")
+    return HttpResponse(f"Resultado: {num1 + num2}")
 
 def suma_formulario(request):
-    return HttpResponse(request, "prueba.html")
+    return render(request, "prueba.html")
 
 def procesar_suma(request):
-    num1 = int(request.POST.get("num1"))
-    num2 = int(request.POST.get("num2"))
-    return HttpResponse(f"Resultado: {num1+num2}")
-    
+    if request.method == "POST":
+        num1 = int(request.POST.get("num1", 0))
+        num2 = int(request.POST.get("num2", 0))
+        return HttpResponse(f"Resultado: {num1 + num2}")
