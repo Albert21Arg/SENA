6d4b47428bdb03f42de319163180faf1e767817b
Albert Taborda <alberttaborda@hotmail.com>
2024-09-12 14:46:01 -0500

    ejercicios django

--- django/sena/educalab/views.py
+++ django/sena/educalab/views.py
@@ -17,7 +17,7 @@ def suma(request, num1, num2):
     return HttpResponse(f"Resultado: {num1+num2}")
 
 def suma_formulario(request):
-    return render(request, "prueba.html")
+    return HttpResponse(request, "prueba.html")
 
 def procesar_suma(request):
     num1 = int(request.POST.get("num1"))
