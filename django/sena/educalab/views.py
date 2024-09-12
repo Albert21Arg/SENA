727cfc3c6942287d595ad3cac1d84ecf488f82ca
Albert Taborda <alberttaborda@hotmail.com>
2024-09-12 07:50:28 -0500

    django

---
+++ django/sena/educalab/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def saludar(request):
+    print("Hola mundo...!")
+    return HttpResponse("Hola SENA..!")
+
+def index(request):
+    return render(request, "index.html")
+
+def saludar_personalizado(request, nombre):
+    return HttpResponse(f"Hola {nombre}")
+
+def suma(request, num1, num2):
+    return HttpResponse(f"Resultado: {num1+num2}")
+
+def suma_formulario(request):
+    return render(request, "prueba.html")
+
+def procesar_suma(request):
+    num1 = int(request.POST.get("num1"))
+    num2 = int(request.POST.get("num2"))
+    return HttpResponse(f"Resultado: {num1+num2}")
+    
---
+++ django/sena/sena/educalab/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+# rutas de la aplicación EducaLab
+
+urlpatterns = [
+    path("saludar/", views.saludar , name="saludar"),
+    path("", views.index, name="index"),
+    path("saludo_per/<str:nombre>/", views.saludar_personalizado, name="saludo_per"),
+    path("suma/<int:num1>/<int:num2>/", views.suma, name="suma"),
+    path("suma_formulario/", views.suma_formulario , name="suma_formulario"),
+    path("procesar_suma/", views.procesar_suma , name="procesar_suma"),
+]
